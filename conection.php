049807c199c119e2fffb64695b67601d6d787684
RenanDeveloper <renantameirao@gmail.com>
2022-11-05 19:37:14 -0300

    adicionado seleção do banco de dados.

--- conection.php
+++ conection.php
@@ -6,9 +6,9 @@ $db = "crud_db";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $db);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+$db = mysqli_select_db($conn, 'crud_db');
 ?>
