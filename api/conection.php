70f015db7153cc523471c48fee612095e799a1b2
RenanDeveloper <renantameirao@gmail.com>
2022-11-07 22:23:53 -0300

    tentativa de hospedagem via Vercel

--- api/conection.php
+++ api/conection.php
@@ -10,5 +10,5 @@ $conn = new mysqli($servername, $username, $password, $db);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$db = mysqli_select_db($conn, 'crud_db');
+
 ?>
