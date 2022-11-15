ee504664d46a6503bc2d46a95d559af521ffd33e
RenanDeveloper <renantameirao@gmail.com>
2022-11-15 11:12:52 -0300

    atualização do menu para sistema de login.

--- navbar.php
+++ navbar.php
@@ -1,4 +1,17 @@
 <nav>
-  <a href="index.php">Home</a>
-  <a href="new-product.php">Cadastrar</a>
+    <a href="index.php">Home</a>
+    <a href="new-product.php">+Produto</a>
+    <?php
+      if(!isset($_SESSION)){
+        session_start();
+        if(isset($_SESSION['id'])){
+          if($_SESSION['admin']){
+            echo "<a href='new-user.php'>+Usuário</a>";
+          }
+          echo "<a href='logout.php'>Sair</a>";
+        }else{
+          echo "<a href='login.php'>Login</a>";
+        }
+      }
+    ?>
 </nav>
