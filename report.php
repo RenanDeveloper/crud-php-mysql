943e6f27fab3dae3675d71dfae52fcc1c00409e4
RenanDeveloper <renantameirao@gmail.com>
2022-12-11 19:07:55 -0300

    Geração de  relatório do dia usando data inicial como parâmetro

---
+++ report.php
@@ -0,0 +1,64 @@
+<?php 
+  include("conection.php");
+  if(isset($_POST['init_date'])){
+    $theDate = $_POST['init_date'];
+  }else{
+    $theDate = date("Y-m-d");
+  }
+  $query = "SELECT * from tb_products INNER JOIN tb_output ON tb_products.id = tb_output.id_product WHERE tb_output.date_output='$theDate'";
+  $result = mysqli_query($conn, $query);
+  
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Estoque</title>
+</head>
+<body>
+  <div class="container">
+    <main>
+      <?php include("navbar.php"); ?>
+      <h1>Relatório</h1>
+        <form action="" method="POST">
+         <label for="init_date">Data inicial: <input type="date" id="init_date" name="init_date"></label><br>
+         <label for="end_date">Data final: <input type="date" id="end_date" name="end_date"></label><br>
+          <input type="submit" id="gerar" value="Gerar relatório">
+        </form> 
+        <table>
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Quantidade</th>
+            <th>Preço</th>
+            <th>Código de barras</th>
+            <th>Baixa</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php
+          while($row = mysqli_fetch_array($result)){
+            if($row['quantity'] > $row['min_quantity']){
+              echo '<tr>';
+            }else if($row['quantity'] == $row['min_quantity']){
+              echo '<tr style="Background-color: orange">';
+            }else{
+              echo '<tr style="Background-color: red">';
+            }
+            echo '
+              <td>'.$row['name'].'</td>
+              <td>'.$row['quantity'].'</td>
+              <td>'.$row['price'].'</td>
+              <td>'.$row['barcode'].'</td>
+              <td>'.$row['quantity_output'].'</td>
+            </tr>';
+          }
+        ?>
+        </tbody>
+      </table>
+    </main>
+  </div>
+</body>
+</html>
