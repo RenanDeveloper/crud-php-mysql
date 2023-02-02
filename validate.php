9e224225696f946b7cb47c7cb4295dc7c0ca8499
RenanDeveloper <renantameirao@gmail.com>
2023-02-01 23:40:15 -0300

    estilização

--- validate.php
+++ validate.php
@@ -1,8 +1,23 @@
-<?php
-if(!isset($_SESSION)){
-  session_start();
-}
-if(!isset($_SESSION['id'])){
-  die("Você não está logado.<p><a href='login.php'>Fazer login</a></p>");
-}
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Estoque</title>
+  <link rel="stylesheet" href="./css/normalize.css">
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+  <?php include("navbar.php"); ?>
+  <div class="container">
+    <?php
+    if(!isset($_SESSION)){
+      session_start();
+    }
+    if(!isset($_SESSION['id'])){
+      die("Você não está logado.<p><a href='login.php'>Fazer login</a></p>");
+    }
+    ?>
+</body>
+</html>
